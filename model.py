6c87f37bf677926a9b2d20662823a891cdc67b38
Emmett Jaakkola <56277507+Emmennater@users.noreply.github.com>
2025-12-10 21:31:06 -0600

    Added replay buffer and completed dynamics model

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ MuZero
 - Learns a latent model of the environment
 - Uses Monte Carlo Tree Search (MCTS)
 - Requires past sequences of observations (frames/states) and actions to be stored
-- Actions are encoded as constant bias planes
+- Actions are encoded as constant bias planes (additional channels to the latent representation)
 - MCTS predicts the next state using the previous state and action
   - Only masks valid actions at the root of the search tree
 - MCTS can proceed past a terminal node, and is expected to return the same terminal state
@@ -48,24 +48,25 @@ import torch.nn.functional as F
 
 STATE_SIZE = 10
 ACTION_SIZE = 3
-SUPPORT_SIZE = 101 # [-50, 50]
+SUPPORT_SIZE = 101 # Categorical reward and value [-50, 50] (see Appendix F of MuZero paper)
 
-def upper_confidence_bound(mcts_score, policy_score, parent_visits, child_visits, c1=1.25, c2=19652):
+def upper_confidence_bound(mcts_score, total_mcts_value, policy_score, parent_visits, child_visits, c1=1.25, c2=19652):
   """
-  Note: mcts_score should be normalized across all actions [0, 1].
+  The mcts_score is normalized across all actions [0, 1].
   
   :param mcts_score: The propogated score from MCTS.
+  :param total_mcts_value: The propogated scores for all actions from the parent node.
   :param policy_score: The score of the action predicted by the policy.
   :param parent_visits: The number of times the parent node has been visited.
   :param child_visits: The number of times the child node has been visited.
   :param c1: The exploration weight.
   :param c2: The exploration decay.
   """
-  return mcts_score + policy_score * np.sqrt(parent_visits) / (1 + child_visits) * (c1 + np.log((parent_visits + c2 + 1) / c2))
+  return mcts_score / total_mcts_value + policy_score * np.sqrt(parent_visits) / (1 + child_visits) * (c1 + np.log((parent_visits + c2 + 1) / c2))
 
 def scale_targets(x, eps=1e-3):
   """
-  Appendix F: Network Architecture says for a value and reward prediction
+  MuZero Appendix F: Network Architecture says for a value and reward prediction
   we scale the targets before we obtain the categorical representations.
 
   :param x: The target (value or reward)
@@ -73,8 +74,45 @@ def scale_targets(x, eps=1e-3):
   """
   return np.sign(x) * (np.sqrt(np.abs(x) + 1) - 1 + eps * x)
 
+class ReplayBuffer:
+  """
+  Stores the trajectories: (prev_state, next_state, action, reward, is_done)
+  """
+  def __init__(self, capacity):
+    self.buffer = []
+    self.capacity = capacity
+
+  def add_trajectory(self, trajectory):
+    """
+    :param trajectory: Comes from the MCTS search
+    """
+    self.buffer.append(trajectory)
+    if len(self.buffer) > self.capacity:
+      self.buffer.pop(0)
+
+  def get_sampling_priority(mcts_value, target_value):
+    """
+    Based on the MuZero Appendix G<br>
+    This function is for choosing the replay sample from the replay buffer to train with.
+    The higher the difference in mcts_value and target_value, the higher the
+    priority (this corresponds to uncertainty).
+    
+    ### Important!
+    To get a probability, normalize all priorities across all replay samples
+    so that you can correct for sampling bias in the future. This is done by scaling
+    the loss by w_i = (1 / N) * (1 / P(i)), where N is the number of replay samples
+    and P(i) is the priority of the ith replay sample.
+
+    :param mcts_value: The search value for the replay sample
+    :param target_value: The target value from the replay sample
+    :param total_mcts_value: The total search value for all replay samples
+    """
+    return np.abs(mcts_value - target_value) # Don't forget to normalize to get the probability!
+
 class PredictionModel(nn.Module):
-  """Two-headed model for predicting policy and value from a state."""
+  """
+  Two-headed model for predicting policy and value from a state.
+  """
   def __init__(self, latent_size=16):
     super().__init__()
     self.fc1 = nn.Linear(STATE_SIZE, latent_size)
@@ -87,11 +125,44 @@ class PredictionModel(nn.Module):
     x = F.relu(self.fc2(x))
     return self.policy(x), self.value(x)
 
+class ResBlock(nn.Module):
+  """
+  This residual block is based on https://arxiv.org/pdf/1603.05027.pdf<br>
+  It is uses the constant scaling method since it is not a CNN.
+
+  :param channels: The number of channels in the input
+  :param alpha: The scaling factor
+  """
+  def __init__(self, channels, alpha=0.2):
+    super().__init__()
+    self.alpha = alpha
+    self.fc1 = nn.Linear(channels, channels)
+    self.fc2 = nn.Linear(channels, channels)
+
+  def forward(self, x):
+    identity = x
+    x = F.relu(self.fc1(x))
+    x = F.relu(self.fc2(x))
+    return identity + x * self.alpha
+
 class DynamicsModel(nn.Module):
   """
   Model Architecture Based On: https://arxiv.org/pdf/1603.05027<br>
-  This model predicts the next state and reward given a state and an action.
+  This model predicts the next state and reward given previous states and actions.
+  Usually, you pass in multiple previous states and actions, but for now we will
+  only pass in a single previous state and action. This may make it harder to train.
+
+  :param latent_size: The number of channels in the latent representation.
+  :param n_blocks: The number of residual blocks.
   """
-  def __init__(self, latent_size=16):
+  def __init__(self, latent_size=16, n_blocks=3):
     super().__init__()
-    self.fc1 = nn.Linear(STATE_SIZE, latent_size)
+    self.first = nn.Linear(STATE_SIZE + ACTION_SIZE, latent_size)
+    self.model = nn.Sequential([ResBlock(latent_size) for _ in range(n_blocks)])
+    self.state = nn.Linear(latent_size, STATE_SIZE)
+    self.reward = nn.Linear(latent_size, SUPPORT_SIZE)
+
+  def forward(self, x):
+    x = F.relu(self.first(x))
+    x = self.model(x)
+    return self.state(x), self.reward(x)
