9b40bc94fc4afe090f209252a9d9f1dbdba2f067
Justin R George <96226288+Justinreji@users.noreply.github.com>
2025-12-12 20:27:14 -0600

    Updated model.py

--- model.py
+++ model.py
@@ -40,6 +40,7 @@ Categorical reward and value
 - Rewards and values are encoded as probability distributions for scores -300 to 300
 - Targets are mapped to this distribution (phi)
 """
+import random
 
 """
 TODO:
@@ -53,6 +54,8 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import Dict, List, Optional
+
 
 MAX_FLOAT = float('inf')
 STATE_SIZE = 10
@@ -75,21 +78,179 @@ class MinMaxStats:
       return (val - self.min) / (self.max - self.min)
     return val
 
+class Action(object):
+
+  def __init__(self, index: int):
+    self.index = index
+
+  def __hash__(self):
+    return self.index
+
+  def __eq__(self, other):
+    return self.index == other.index
+
+  def __gt__(self, other):
+    return self.index > other.index
+
+
+class Player(object):
+  pass
+
+class Node:
+  """
+  Node in the MCTS search tree.
+  """
+  def __init__(self, prior: float):
+    self.visit_count = 0
+    self.to_play = -1
+    self.prior = prior
+    self.value_sum = 0
+    self.children = {}
+    self.hidden_state = None
+    self.reward = 0
+
+  def expanded(self) -> bool:
+    return len(self.children) > 0
+
+  def value(self) -> float:
+    if self.visit_count == 0:
+      return 0
+    return self.value_sum / self.visit_count
+
+class ActionHistory(object):
+  """Simple history container used inside the search.
+
+  Only used to keep track of the actions executed.
+  """
+
+  def __init__(self, history: List[Action], action_space_size: int):
+    self.history = list(history)
+    self.action_space_size = action_space_size
+
+  def clone(self):
+    return ActionHistory(self.history, self.action_space_size)
+
+  def add_action(self, action: Action):
+    self.history.append(action)
+
+  def last_action(self) -> Action:
+    return self.history[-1]
+
+  def action_space(self) -> List[Action]:
+    return [Action(i) for i in range(self.action_space_size)]
+
+  def to_play(self) -> Player:
+    return Player()
+
+class Environment(object):
+  """The environment MuZero is interacting with."""
+
+  def step(self, action):
+    pass
+
+class Game(object):
+  """A single episode of interaction with the environment."""
+
+  def __init__(self, action_space_size: int, discount: float):
+    self.environment = Environment()  # Game specific environment.
+    self.history = []
+    self.rewards = []
+    self.child_visits = []
+    self.root_values = []
+    self.action_space_size = action_space_size
+    self.discount = discount
+
+  def terminal(self) -> bool:
+    # Game specific termination rules.
+    pass
+
+  def legal_actions(self) -> List[Action]:
+    # Game specific calculation of legal actions.
+    return []
+
+  def apply(self, action: Action):
+    reward = self.environment.step(action)
+    self.rewards.append(reward)
+    self.history.append(action)
+
+  def store_search_statistics(self, root: Node):
+    sum_visits = sum(child.visit_count for child in root.children.values())
+    action_space = (Action(index) for index in range(self.action_space_size))
+    self.child_visits.append([
+        root.children[a].visit_count / sum_visits if a in root.children else 0
+        for a in action_space
+    ])
+    self.root_values.append(root.value())
+
+  def make_image(self, state_index: int):
+    # Game specific feature planes.
+    return []
+
+  def make_target(self, state_index: int, num_unroll_steps: int, td_steps: int,
+                  to_play: Player):
+    # The value target is the discounted root value of the search tree N steps
+    # into the future, plus the discounted sum of all rewards until then.
+    targets = []
+    for current_index in range(state_index, state_index + num_unroll_steps + 1):
+      bootstrap_index = current_index + td_steps
+      if bootstrap_index < len(self.root_values):
+        value = self.root_values[bootstrap_index] * self.discount**td_steps
+      else:
+        value = 0
+
+      for i, reward in enumerate(self.rewards[current_index:bootstrap_index]):
+        value += reward * self.discount**i  # pytype: disable=unsupported-operands
+
+      # For simplicity the network always predicts the most recently received
+      # reward, even for the initial representation network where we already
+      # know this reward.
+      if current_index > 0 and current_index <= len(self.rewards):
+        last_reward = self.rewards[current_index - 1]
+      else:
+        last_reward = 0
+
+      if current_index < len(self.root_values):
+        targets.append((value, last_reward, self.child_visits[current_index]))
+      else:
+        # States past the end of games are treated as absorbing states.
+        targets.append((0, last_reward, []))
+    return targets
+
+  def to_play(self) -> Player:
+    return Player()
+
+  def action_history(self) -> ActionHistory:
+    return ActionHistory(self.history, self.action_space_size)
+
 class ReplayBuffer:
   """
   Stores the trajectories: (prev_state, next_state, action, reward, is_done)
   """
-  def __init__(self, capacity):
+  def __init__(self, capacity, batch_size):
     self.buffer = []
+    self.batch_size = batch_size
     self.capacity = capacity
 
-  def add_trajectory(self, trajectory):
-    """
-    :param trajectory: Comes from the MCTS search
-    """
-    self.buffer.append(trajectory)
+  def save_games(self, game):
+    self.buffer.append(game)
     if len(self.buffer) > self.capacity:
       self.buffer.pop(0)
+
+  def sample_batch(self, num_unroll_steps: int, td_steps: int):
+      games = [self.sample_game() for _ in range(self.batch_size)]
+      game_pos = [(g, self.sample_position(g)) for g in games]
+      return [(g.make_image(i), g.history[i:i + num_unroll_steps],
+               g.make_target(i, num_unroll_steps, td_steps, g.to_play()))
+              for (g, i) in game_pos]
+
+  def sample_game(self) -> Game:
+    # Sample game from buffer either uniformly or according to some priority.
+    return random.choice(self.buffer)
+
+  def sample_position(self, game) -> int:
+    # Sample position from game either uniformly or according to some priority.
+    T = len(game.history)
+    return random.randint(0, T - 1)
 
   def get_sampling_priority(mcts_value, target_value):
     """
@@ -182,26 +343,6 @@ class DynamicsModel(nn.Module):
     state = (state - state_mins) / (state_maxs - state_mins + 1e-6)
     return state, reward
 
-class Node:
-  """
-  Node in the MCTS search tree.
-  """
-  def __init__(self, prior: float):
-    self.visit_count = 0
-    self.to_play = -1
-    self.prior = prior
-    self.value_sum = 0
-    self.children = {}
-    self.hidden_state = None
-    self.reward = 0
-
-  def expanded(self) -> bool:
-    return len(self.children) > 0
-
-  def value(self) -> float:
-    if self.visit_count == 0:
-      return 0
-    return self.value_sum / self.visit_count
 
 class MCTS:
   """
